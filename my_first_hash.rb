1e0579aa827ef4839895e9d09e0a12df14de07f9
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-21 03:05:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,7 +6,7 @@ require 'pry'
 
 
 
-  def shipping_manifest()
+  def shipping_manifest(hash)
     the_manifest = {"whale bone corsets" => 2, "oil paintings" => 5, "porcelain vases" => 3}
     return hash
   end
