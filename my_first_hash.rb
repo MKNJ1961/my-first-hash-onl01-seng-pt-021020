ac9eda587fc8f56f7595de82d516a417f0c8485e
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-21 02:44:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,7 +3,7 @@
   end
 
   def shipping_manifest()
-    the_manifest = {"whale_bone_corsets" => 5, "porcelin_vases" => 2, "oil_painting" => 3}
+    the_manifest = {"whale_bone_corsets" => 5, "porcelain_vases" => 2, "oil_paintings" => 3}
   end
 
   # set a variable called `the_manifest`, equal to a hash
